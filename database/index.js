4018ecb7cfd0b25625ecba253b19cc5955e73472
Caz <cazyw@users.noreply.github.com>
2018-11-27 20:57:30 +1100

    add docker and buildkite config files; modify test files to work with buildkite and docker

--- database/index.js
+++ database/index.js
@@ -1,8 +1,10 @@
 var mongoose = require('mongoose');
-const environment = process.env.NODE_ENV || 'PRODUCTION';
-let db = `node-api-${environment}`;
 
-mongoose.connect(`mongodb://localhost/${db}`, { useNewUrlParser: true });
+const db = process.env.NODE_ENV || 'PRODUCTION';
+const hostName = process.env.NODE_ENV === 'DOCKER' ? 'mongo' : 'localhost';
+const databaseUrl = `mongodb://${hostName}:27017/node-api-${db}`;
+
+mongoose.connect(databaseUrl, { useNewUrlParser: true });
 
 var movieDatabaseSchema = mongoose.model('Movie', new mongoose.Schema({
 	title: String,
